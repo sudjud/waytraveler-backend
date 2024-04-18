f28d0bcd5671d80eabc4724f30822bbdc65fd891
Khalid <sudjud@bk.ru>
2024-04-19 01:30:59 +0200

    updated index.js

--- index.js
+++ index.js
@@ -2,44 +2,56 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
 const app = express();
 require('dotenv').config();
 
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-  next();
-});
+// Настройка заголовков безопасности
 app.use(helmet());
-app.use(express.json());
-app.use(require('morgan')(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'));
-app.use(express.urlencoded({
-  extended: true
-}));
-app.use(fileUpload());
+
+// Разрешение кросс-доменных запросов
 app.use(cors());
-app.use(require("./routes/index.js"));
+
+// Обработка JSON и urlencoded данных
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Логирование
+app.use(require('morgan')(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'));
+
+// Загрузка файлов
+app.use(fileUpload());
+
+// Установка кросс-оригин ресурс политики для всех ответов
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+});
+
+// Статические файлы и CORS для изображений
 app.use('/uploads/images', cors(), express.static('uploads/images'));
 
-const PORT = process.env.PORT || 3000;
+// Подключение маршрутов
+app.use(require("./routes/index.js"));
 
-mongoose.
-connect(process.env.MONGO_URL, {
+// Подключение к MongoDB
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Сервер запущен на localhost:${PORT}`);
-    })
-  })
-  .catch(error => console.log('Ошибка при соединении с сервером:', error));
+}).then(() => {
+    // Запуск сервера после успешного подключения к БД
+    app.listen(process.env.PORT, () => {
+        console.log(`Сервер запущен на localhost:${process.env.PORT}`);
+    });
+}).catch(error => console.log('Ошибка при соединении с сервером:', error));
 
+// Обработка ошибки 404
 app.use((req, res, next) => {
-  res.status(404).send('Страница не найдена');
+    res.status(404).send('Страница не найдена');
 });
 
+// Обработка остальных ошибок
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Что-то поломалось:(');
+    console.error(err.stack);
+    res.status(500).send('Что-то поломалось:(');
 });
